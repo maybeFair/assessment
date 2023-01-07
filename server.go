54c5a8636b992370f7d1d3b2e26355d1ba2bb2e4
Noppawong Somon <idkexotic@gmail.com>
2023-01-07 23:48:39 +0700

    story3

--- server.go
+++ server.go
@@ -93,6 +93,69 @@ func getIdExpenseHandlers(c echo.Context) error {
 	return c.JSON(http.StatusOK, resBody)
 
 }
+func getAllexpensesHandler(c echo.Context) error {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	id := c.Param("id")
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, model.Errmsg{Message: "Id is not found value"})
+	}
+	result := model.Resbody{}
+	row := db.QueryRow("SELECT id, title, amount, note, tags FROM expenses WHERE id=$1", id)
+
+	err1 := row.Scan(&result.Id, &result.Title, &result.Amount, &result.Note, pq.Array(&result.Tags))
+	if err1 != nil {
+		return c.JSON(http.StatusInternalServerError, model.Errmsg{Message: err.Error()})
+	}
+
+	resBody := model.Resbody{
+		Id:     result.Id,
+		Title:  result.Title,
+		Amount: result.Amount,
+		Note:   result.Note,
+		Tags:   result.Tags,
+	}
+	defer db.Close()
+
+	return c.JSON(http.StatusOK, resBody)
+
+}
+func updateExpenseHandler(c echo.Context) error {
+	id := c.Param("id")
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, model.Errmsg{Message: "Id is not found value"})
+	}
+	var body model.Reqbody
+	err := c.Bind(&body)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, model.Errmsg{Message: err.Error()})
+	}
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	result := model.Resbody{}
+	row := db.QueryRow("UPDATE expenses SET title=$2, amount=$3, note=$4, tags=$5 WHERE id=$1 RETURNING *",
+		id, body.Title, body.Amount, body.Note, pq.Array(body.Tags))
+
+	err = row.Scan(&result.Id, &result.Title, &result.Amount, &result.Note, pq.Array(&result.Tags))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, model.Errmsg{Message: err.Error()})
+
+	}
+	resBody := model.Resbody{
+		Id:     result.Id,
+		Title:  result.Title,
+		Amount: result.Amount,
+		Note:   result.Note,
+		Tags:   result.Tags,
+	}
+	return c.JSON(http.StatusOK, resBody)
+
+}
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -105,6 +168,8 @@ func main() {
 	}))
 	e.POST("/expenses", createExpenseHandler)
 	e.GET("/expenses/:id", getIdExpenseHandlers)
+	e.GET("/expenses", getAllexpensesHandler)
+	e.PUT("/expenses/:id", updateExpenseHandler)
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
